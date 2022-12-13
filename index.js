434a359ca85a5f79b87312ea9d3975b3e4e80d5d
Dan <35927536+dan-mba@users.noreply.github.com>
2022-12-13 08:35:43 -0500

    fix background color

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import icons from './icons.json' assert { type: "json" };
 
 const ccc = new ColorContrastChecker();
 const fontSize = 16;
-const bgColor = "#000000"
+const bgColor = "#ffffff"
 
 for (let key in icons) {
-  if (ccc.isLevelAA(icons[key], bgColor, fontSize)) {
+  if (!ccc.isLevelAA(icons[key], bgColor, fontSize)) {
     console.log(`${key} does not meet AA contrast`)
   }
 }
